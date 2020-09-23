1880716d2b5066ce4124905a0cef588e3dba3815
STB a.k.a. Satomaru <erynenadan@gmail.com>
2020-09-24 03:25:07 +0900

    rss-parserを用いるように変更し、仕様を一新。

--- google-news.js
+++ google-news.js
@@ -1,21 +1,121 @@
-const https = require('./https.js');
-const xmlParser = require('fast-xml-parser');
+const RssParser = require('rss-parser');
+const parser = new RssParser();
 
-const technologyTopicsUrl =
-  'https://news.google.com' +
-  '/news/rss/headlines/section/topic/TECHNOLOGY' +
-  '?hl=ja&gl=JP&ceid=JP:ja';
+function resolveUrl(category) {
+  return 'https://news.google.com' +
+    '/news/rss/headlines/section/topic/' + category +
+    '?hl=ja&gl=JP&ceid=JP:ja';
+}
 
 /**
- * GoogleニュースのRSSから、技術記事のトピックを取得します。
- *
- * @module getTechnologyTopics
- * @param {number} [length] - 記事の取得件数
- * @return {Promise} - 技術記事のJSON配列
+ * トピックを管理します。
  */
-module.exports.getTechnologyTopics = length =>
-  https.get(technologyTopicsUrl)
-    .then(xml => {
-      const topics = xmlParser.parse(xml).rss.channel.item;
-      return (length !== undefined) ? topics.slice(0, length) : topics;
+module.exports.Topic = class Topic {
+
+  /** トピックのカテゴリー。 */
+  category = null;
+
+  /** トピックのリスト。 */
+  items = [];
+
+  /** トピックのインデックス。 */
+  index = 0;
+
+  /**
+   * カレントのトピック。
+   */
+  get current() {
+    return (this.index >= 0 && this.index < this.size) ? this.items[this.index] : null;
+  }
+
+  /**
+   * トピックの個数。
+   */
+  get size() {
+    return this.items.length;
+  }
+
+  /**
+   * カレントのトピック以降に残っているトピックの個数。
+   */
+  get remain() {
+    return (this.size === 0) ? 0 : this.size - this.index - 1;
+  }
+
+  /**
+   * カレントのトピックの説明を作成します。
+   *
+   * @return {string} - カレントのトピックの文字列表現
+   */
+  describe() {
+    if (this.idDownloaded()) {
+      const json = JSON.stringify(this.current, null, 2);
+      return `${this.category} #${this.index} ${json}`;
+    } else {
+      return '(not downloaded)';
+    }
+  }
+
+  /**
+   * トピックがダウンロード済であることを判定します。
+   *
+   * @return トピックがダウンロード済である場合はtrue
+   */
+  idDownloaded() {
+    return this.category !== null;
+  }
+
+  /**
+   * 全てのトピックを消去します。
+   */
+  reset() {
+    this.category = null;
+    this.items = [];
+    this.index = 0;
+  }
+
+  /**
+   * トピックをダウンロードします。
+   *
+   * @param {string} category - トピックのカテゴリー名
+   * @return {Promise} - このオブジェクト自身
+   */
+  download(category) {
+    this.reset();
+    this.category = category;
+
+    return parser.parseURL(resolveUrl(category)).then(feed => {
+      this.items = feed.items.map(item => ({
+        title: item.title,
+        pubDate: item.pubDate
+      }));
+
+      return this;
     });
+  }
+
+  /**
+   * カレントのトピックを変更します。
+   *
+   * @param {number} index - トピックのインデックス
+   * @return {boolean} - 変更に成功した場合はtrue
+   */
+  jump(index) {
+    if (index < 0 || index >= this.size) {
+      return false;
+    }
+
+    this.index = index;
+    return true;
+  }
+
+  /**
+   * カレントのトピックをオフセット移動します。
+   *
+   * @param {number} offset - 移動量
+   * @return {boolean} - 移動に成功した場合はtrue
+   */
+  move(offset) {
+    return this.jump(this.index + offset);
+  }
+}
--- index.js
+++ index.js
@@ -1,5 +1,69 @@
-const googleNews = require('./google-news.js');
+const Topic = require('./google-news').Topic;
+const topic = new Topic();
 
-googleNews.getTechnologyTopics(5)
-  .then(topics => topics.forEach(topic => console.log('「%s」\n', topic.title)))
-  .catch(error => console.error(error));
+/** トピックのカテゴリーリスト。 */
+const categoryList = [
+  'WORLD',
+  'NATION',
+  'BUSINESS',
+  'TECHNOLOGY',
+  'ENTERTAINMENT',
+  'SPORTS',
+  'SCIENCE',
+  'HEALTH'
+];
+
+/**
+ * カテゴリーの入力要求を表示します。
+ */
+function promptCategory() {
+  categoryList.forEach((value, index) => console.log('%d..%s', index, value));
+  process.stdout.write('\nchoose category> ');
+}
+
+/**
+ * カテゴリーの説明、および操作の入力要求を表示します。
+ */
+function showTopicAndPrompt() {
+  console.log(topic.describe());
+  process.stdout.write(`\nmore ${topic.remain} topic(s). q)uit or next> `);
+}
+
+/**
+ * トピックがダウンロード済である場合の入力後処理を行います。
+ */
+function whenDownloaded(input) {
+  if (input === 'q') {
+    topic.reset();
+    promptCategory();
+  } else {
+    topic.move(1);
+    showTopicAndPrompt();
+  }
+}
+
+/**
+ * トピックがダウンロード済でない場合の入力後処理を行います。
+ */
+function whenNotDownloaded(input) {
+  const index = parseInt(input);
+
+  if (index >= 0 && index < categoryList.length) {
+    topic.download(categoryList[index]).then(() => showTopicAndPrompt());
+  } else {
+    promptCategory();
+  }
+}
+
+promptCategory();
+
+process.stdin.on('data', buffer => {
+  const input = buffer.toString().replace(/[\s\0]+$/, '');
+  console.log();
+
+  if (topic.idDownloaded()) {
+    whenDownloaded(input);
+  } else {
+    whenNotDownloaded(input);
+  }
+});
